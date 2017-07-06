921f656737968e6380e446d452d0eac97bebb431
Bhuvana <bhuvanatn@gmail.com>
2017-07-06 18:43:30 +1000

    passing and using data with event buinding

--- myfirstAngular4App/src/app/servers/servers.component.ts
+++ myfirstAngular4App/src/app/servers/servers.component.ts
@@ -6,21 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-allowNewServer = false;
-serverCreationStatus = 'No server was created!';
-  constructor() { 
-    setTimeout(()=>{
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
   ngOnInit() {
   }
- OnCreateServer(){
-this.serverCreationStatus = 'Server was created!';
- }
- onUpdateServerName(event){
- console.log("I am eventvalue:",event);
- 
- }
+  OnCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+  onUpdateServerName(event) {
+    // console.log("I am eventvalue:", event.target.value);
+    // this.serverName = event.target.value;
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }

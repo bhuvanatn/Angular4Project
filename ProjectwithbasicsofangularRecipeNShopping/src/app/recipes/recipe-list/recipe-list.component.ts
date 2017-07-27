bfc6dde291ea25537da159c4ff35179d042306b5
Bhuvana <bhuvanatn@gmail.com>
2017-07-27 16:52:20 +1000

    property with type in recipe-list

--- ProjectwithbasicsofangularRecipeNShopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ProjectwithbasicsofangularRecipeNShopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A new Recipe', 'This is delicious Recipe','https://cdn.lifestyle.com.au/cache/400x225/recipes/thumbnails/IceCreamRecipeCollection.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
